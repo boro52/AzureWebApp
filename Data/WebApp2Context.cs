b1e2c0aa71e68ea5ebe421e1ea50e07f2c8169e5
Damian Wolkowicz <damianwolkowicz9@gmail.com>
2021-02-03 23:40:39 +0100

    First revision of app

---
+++ Data/WebApp2Context.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using WebApp2.Models;
+
+namespace WebApp2.Data
+{
+    public class WebApp2Context : DbContext
+    {
+        public WebApp2Context (DbContextOptions<WebApp2Context> options)
+            : base(options)
+        {
+            Database.EnsureCreated();
+        }
+
+        public DbSet<WebApp2.Models.Employee> Employee { get; set; }
+    }
+}
